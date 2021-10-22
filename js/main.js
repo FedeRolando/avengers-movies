5f240a9a7c9ef087506e94a15ca5a62844013b90
Fede Rolando <rolandofedericog@gmail.com>
2021-10-22 11:57:00 -0300

    commit all files

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const d = document,
+c = console;
+
+const $sectionMovies = d.getElementById("sectionMovies"),
+$modalMovie = d.getElementById("modalMovie"),
+$modalContent = d.getElementById("modalContent"),
+$closeModal = d.querySelector(".closeModal"),
+$movieTitle = d.getElementById("movieTitle"),
+$cl1 = d.querySelector(".cl1"),
+$movieImg = d.getElementById("movieImg"),
+$btnWatch =  d.getElementById("btnWatch"),
+$sinopsis = d.getElementById("sinopsis"),
+$video = d.getElementById("video");
+
+let selectedMovie;
+
+d.addEventListener("DOMContentLoaded",()=>{
+  let fragment = d.createDocumentFragment()
+  for(let i of movies){
+    let movie = d.createElement("div"); 
+    movie.innerHTML = `
+        <img src="${i.img}" alt="${i.title}">
+      `
+    movie.classList.add("movie");
+    movie.addEventListener("click",()=>{
+      selectedMovie = i;
+      $movieTitle.innerText = `${i.title}`;
+      $movieTitle.classList.remove("d-none");
+      $movieImg.setAttribute("src",i.img);
+      $cl1.classList.remove("d-none");
+      $sinopsis.innerHTML = `<h3>Sinopsis</h3> ${i.sinopsis}`;
+      $sinopsis.classList.remove("d-none");
+
+      $modalMovie.classList.remove("d-none");
+    })
+    fragment.appendChild(movie);
+  }
+  $sectionMovies.appendChild(fragment);
+});
+
+$closeModal.addEventListener("click",()=>{
+  $modalMovie.classList.add("d-none");
+  $video.classList.add("d-none");
+})
+
+$modalMovie.addEventListener("click",(e)=>{
+  if(e.target.matches("#modalMovie")) {
+    $modalMovie.classList.add("d-none");
+    $video.classList.add("d-none");
+  }
+})
+
+$btnWatch.addEventListener("click",()=>{
+  $video.setAttribute("src",selectedMovie.video);
+  $movieTitle.classList.add("d-none");
+  $cl1.classList.add("d-none");
+  $sinopsis.classList.add("d-none");
+  $video.classList.remove("d-none");
+  console.log(selectedMovie.video);
+  selectedMovie = null;
+})
---
+++ js/movies.js
@@ -0,0 +1,33 @@
+const vengadores = {
+  title: "Vengadores",
+  year: 2012,
+  sinopsis: "<p>Cuando un enemigo inesperado amenaza con poner en peligro la seguridad mundial, Nicholas Fury, Director de la agencia internacional para el mantenimiento de la paz, conocida con el nombre de S.H.I.E.L.D., necesita encontrar urgentemente un equipo que salve al mundo del mayor de los desastres. Así empieza una búsqueda por todo el mundo para reclutar personal.</p>",
+  img: "img/movies/vengadores-min.png",
+  video: "video/vengadores.mp4"
+}
+
+const vengadoresEraDeUltron = {
+  title: "Vengadores: Era de ultron",
+  year: 2015,
+  sinopsis: "<p>Cuando sale a la luz el misterioso Ultrón, el equipo se reúne para detener al aterrador villano tecnológico empeñado en la extinción de la humanidad. Por el camino, se enfrentan a dos poderosos y misteriosos recién llegados, Wanda Maximoff y Pietro Maximoff, así como a un viejo amigo que ha tomado una nueva forma con el nombre de Visión.</p><p>El orden del mundo pende de un hilo y Los Vengadores deben impedir que Ultrón lleve a cabo sus aterradores planes.</p>",
+  img: "img/movies/vengadores-eraDeUltron-min.jpg",
+  video: "video/vengadores-eraDeUltron.mp4"
+}
+
+const vengadoresInfinityWar = {
+  title: "Vengadores: Infinity war",
+  year: 2018,
+  sinopsis: "<p>Infinity War crea un viaje cinematográfico sin precedentes durante diez años y que abarca todo el Universo Marvel. Trae a la pantalla el enfrentamiento definitivo y más sangriento de todos los tiempos.</p><p>Los Vengadores y sus superhéroes deben estar dispuestos a sacrificar todo en un intento de derrotar al poderoso Thanos, antes de que sus planes de devastación y ruina pongan fin al universo.</p>",
+  img: "img/movies/vengadores-InfinityWar-min.png",
+  video: "video/vengadores-InfinityWar.mp4"
+}
+
+const vengadoresEndgame = {
+  title: "Vengadores: Endgame",
+  year: 2019,
+  sinopsis: "<p>Después de los eventos devastadores de ‘Avengers: Infinity War’, el universo está en ruinas debido a las acciones de Thanos.</p><p>Con la ayuda de los aliados que quedaron, los Vengadores deben reunirse una vez más para deshacer sus acciones y restaurar el orden en el universo de una vez por todas, sin importar cuáles son las consecuencias que pueda tener.</p>",
+  img: "img/movies/vengadores-endgame-min.jpg",
+  video: "video/vengadores-endgame.mp4"
+}
+
+const movies = [vengadores,vengadoresEraDeUltron,vengadoresInfinityWar,vengadoresEndgame];
